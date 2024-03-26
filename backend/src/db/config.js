009e25b64ab266202f62bda6d5f561590760049e
Zain Ul Abedin <m.zainulabedinn@gmail.com>
2024-03-26 22:09:53 +0500

    Render-db-integration

--- backend/src/db/config.js
+++ backend/src/db/config.js
@@ -1,19 +1,24 @@
-import {Sequelize} from "sequelize"
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('FinalProject', 'postgres', 'Abcd1234', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });
-
-
-  const connectDb=async() =>{
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
+const sequelize = new Sequelize('fundraisingnetwork', 'fundraisingadmin', 'Tv4j5BrATsQcAoPKVqTLgeprGzSEzC8M', {
+  host: 'dpg-co19otgl6cac73cqe1fg-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false 
       }
   }
+});
+
+const connectDb = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
 export default sequelize;
-export {connectDb};
+export { connectDb };

6ab2fb974b96f4a0390c7f5e16d839817e720909
RafiaMoazum <rafiamoazum@gmail.com>
2024-03-27 11:15:48 +0500

    imageadded and stored

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./styles/HomePage.css"
+
 import { NavLink } from "react-router-dom";
+
+const BackendUrl = 'http://localhost:3000';
 
 const HomePage = () => {
 
@@ -29,7 +32,7 @@ const HomePage = () => {
             {causes.map((element) => (
                 <NavLink to={`/CauseDetailsPage/${element.id}`} style={{ textDecoration: 'none' , color:"black"}}>
                 <div key={element.id} className="card">
-                <img className="picture" src={element.image} alt="image"/>
+                <img className="picture" src={`${BackendUrl}/${element.image}`} alt="image"/>
                     <p>Title: {element.title || "N/A"}</p> 
                     <p>Details: {element.details || "N/A"}</p> 
                     <p>Category: {element.category || "N/A"}</p> 
@@ -43,3 +46,5 @@ const HomePage = () => {
 }
  
 export default HomePage;
+
+{/* <img className="picture" src={`../../../backend/${element.image}`} alt="image"/> */}

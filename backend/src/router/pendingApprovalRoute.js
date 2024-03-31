ada5119db5acd7657368479f0e045034eccbe634
RafiaMoazum <rafiamoazum@gmail.com>
2024-03-31 23:26:50 +0500

    middleware

--- backend/src/router/pendingApprovalRoute.js
+++ backend/src/router/pendingApprovalRoute.js
@@ -1,12 +1,12 @@
 import {Router} from "express"
 import { upload } from "../multer/multerConfig.js"
 import PendingApprovalController from "../controller/cause/pendingApprovalController.js"
-
+import validateUser from "../middleware/validateUser.js"
 
 
 const pendingApprovalRouter=Router();
 
-pendingApprovalRouter.post("/addCause", upload.single('image'), PendingApprovalController.addCause);
+pendingApprovalRouter.post("/addCause",upload.single('image'), PendingApprovalController.addCause);
 pendingApprovalRouter.get("/getAllPendingCauses", PendingApprovalController.getAllPendingCauses)
 
 

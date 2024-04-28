d5c9375931f790ff1681f05af57c34aba0e6b734
Zain Ul Abedin <m.zainulabedinn@gmail.com>
2024-04-29 01:32:01 +0500

    style/fixed

--- frontend/src/components/AnimatedSection.jsx
+++ frontend/src/components/AnimatedSection.jsx
@@ -7,22 +7,20 @@ const AnimatedSection = () => {
 
     const navigate=useNavigate();
     return (
-        <Container fluid>
-        <div className='container' style={{width:"100%"}}>
-            <div className="box1">
-                <p className='heading' >Donate Now</p>
-                <div className='text'>Be the light in someone's darkness.</div>
-                 <div className='text'>Your support makes a world of difference</div>
+        <div className='animatedSection'> 
+        <div className="box1">
+            <p className='heading'>Donate Now</p>
+            <div className='text'>Be the light in someone's darkness.</div>
+            <div className='text'>Your support makes a world of difference.</div>
             <button className='buttonn' onClick={() => navigate("/AllCauses")}>Donate Now</button>
-            </div>
-            <div className="box2">
-                <p className='heading'  >Apply for donation</p>
-                <div className='text'> Don't face it alone.</div>
-                <div className='text'>Apply for assistance and let us stand by your side.</div>
-                <button className='buttonn' onClick={() => navigate("/addCausePage")}>Apply Now</button>
-            </div>
         </div>
-        </Container>
+        <div className="box2">
+            <p className='heading'>Apply for donation</p>
+            <div className='text'>Don't face it alone.</div>
+            <div className='text'>Apply for assistance and let us stand by your side.</div>
+            <button className='buttonn' onClick={() => navigate("/addCausePage")}>Apply Now</button>
+        </div>
+    </div>
     );
 }
 
